c576f395e4a6301c52bcdea32f56ebf395dbd619
AnastasiyaShved <pav159208@gmail.om>
2023-08-24 12:43:40 +0300

    Project created

---
+++ HW15_SignInUpApp/HW15_SignInUpApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW15_SignInUpApp
+//
+//  Created by Apple on 24.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
